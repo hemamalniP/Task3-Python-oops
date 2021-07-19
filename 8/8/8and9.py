1f1135ff5ecef500f6b21d09e06e5ce2a4f707f8
hemamalniP <87513027+hemamalniP@users.noreply.github.com>
2021-07-19 14:44:45 +0530

    que8 /9.py
    Write a Python class named Student with two attributes student_id, student_name. Add a new attribute student_class. Create a function to display the entire attribute and their values in Student class.

---
+++ 8/8/8and9.py
@@ -0,0 +1,9 @@
+class stud:
+    stud_id='1705012'
+    name='Hemamalni'
+    def disp():
+        print("Student id:",{stud.stud_id})
+        print("Student name:",{stud.name})
+        
+stud.disp()
+
